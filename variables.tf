bb275cf9de903dbf88a12bb1681100a6070d8bd7
bendwyer <17102207+bendwyer@users.noreply.github.com>
2025-09-03 20:53:33 +0000

    fix: Switch null default values to false

--- variables.tf
+++ variables.tf
@@ -3,21 +3,26 @@ variable "zitadel_project_name" {
   type        = string
 }
 
+variable "zitadel_has_project_check" {
+  default     = false
+  description = "Checks if user's org has permission to this project."
+  type        = bool
+}
+
+variable "zitadel_org_id" {
+  description = "Organization ID."
+  type        = string
+}
+
 variable "zitadel_project_role_assertion" {
-  default     = null
+  default     = false
   description = "Describes if user roles should be added to token."
   type        = bool
 }
 
 variable "zitadel_project_role_check" {
-  default     = null
+  default     = false
   description = "Checks if user has at least one role in this project."
-  type        = bool
-}
-
-variable "zitadel_has_project_check" {
-  default     = null
-  description = "Checks if user's org has permission to this project."
   type        = bool
 }
 
@@ -37,19 +42,19 @@ variable "zitadel_post_logout_redirect_uris" {
 }
 
 variable "access_token_role_assertion" {
-  default     = null
+  default     = false
   description = "Access token role assertion."
   type        = bool
 }
 
 variable "zitadel_id_token_role_assertion" {
-  default     = null
+  default     = false
   description = "ID token role assertion."
   type        = bool
 }
 
 variable "zitadel_id_token_userinfo_assertion" {
-  default     = null
+  default     = false
   description = "ID token user info assertion."
   type        = bool
 }
@@ -57,4 +62,16 @@ variable "zitadel_id_token_userinfo_assertion" {
 variable "zitadel_additional_origins" {
   description = "List of additional origins."
   type        = list(string)
+}
+
+variable "zitadel_skip_native_app_success_page" {
+  default     = false
+  description = "Skips successful login page on native apps and directly redirect the user to the callback."
+  type        = bool
+}
+
+variable "zitadel_dev_mode" {
+  default     = false
+  description = "Dev mode."
+  type        = bool
 }
